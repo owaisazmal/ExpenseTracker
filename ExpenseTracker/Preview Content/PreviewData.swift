3931ccc0404e43c33c6c2ccffc7befa0b2a3934b
owaisazmal <owaiskhan461@gmail.com>
2024-07-01 08:56:04 -0700

    Ext. updated

---
+++ ExpenseTracker/Preview Content/PreviewData.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewData.swift
+//  ExpenseTracker
+//
+//  Created by Khan, Owais on 6/24/24.
+//
+
+import Foundation
---
+++ ExpenseTracker/TransactionRow.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionRow.swift
+//  ExpenseTracker
+//
+//  Created by Khan, Owais on 6/24/24.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionRow()
+}
