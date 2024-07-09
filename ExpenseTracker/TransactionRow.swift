8a52bf98533ff7c17c934a7fd59cc80977d0dde5
owaisazmal <owaiskhan461@gmail.com>
2024-07-08 22:28:19 -0700

    Signed Ammount

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct TransactionRow: View {
+    var transaction: Transaction
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20){
+            VStack(alignment: .leading, spacing: 6){
+                
+                //Transaction Merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+                
+                //Transaction Category
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(1)
+                
+                //Transaction Date
+                Text(transaction.dateParsed, format: .dateTime.year().month().day())
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            
+            Spacer()
+            //Transaction Ammount:
+            Text(transaction.signedAmount, format: .currency(code: "USD"))
+                .bold()
+                .foregroundColor(transaction.type == TransactionType.credit.rawValue ?  Color.text: .primary)
+        }
+        .padding([.top, .bottom], 8)
     }
 }
-
-#Preview {
-    TransactionRow()
+struct TransactionRo_wPreviews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TransactionRow(transaction: transactionPreviewData)
+            TransactionRow(transaction: transactionPreviewData)
+                .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }
+        
+    }
 }
