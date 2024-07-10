fad0b7d1de2d8c52ac0f475d22f8d7a25c4c2fff
owaisazmal <owaiskhan461@gmail.com>
2024-07-10 16:39:14 -0700

    Category Listing

---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Khan, Owais on 7/10/24.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentTransactionList()
+}
---
+++ ExpenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionListViewModel.swift
+//  ExpenseTracker
+//
+//  Created by Khan, Owais on 7/10/24.
+//
+
+import Foundation
