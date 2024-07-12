8f3096ca8f82055b9938f65ae0a5e5d1ff463961
owaisazmal <owaiskhan461@gmail.com>
2024-07-12 14:54:38 -0700

    working on Chart

---
+++ ExpenseTracker/TransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Khan, Owais on 7/12/24.
+//
+
+import SwiftUI
+
+struct TransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionList()
+}
